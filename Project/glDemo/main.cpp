921facc602c283f7e8571300f3f99dbb73518b39
Makaveli-wls <liambarrettwls@gmail.com>
2024-01-02 17:46:49 +0000

    Window setup

---
+++ Project/glDemo/main.cpp
@@ -0,0 +1,129 @@
+
+#include "core.h"
+
+
+// global variables
+
+// Window size
+const unsigned int initWidth = 512;
+const unsigned int initHeight = 512;
+
+// Function prototypes
+void renderScene();
+void resizeWindow(GLFWwindow* window, int width, int height);
+void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
+void updateScene();
+
+
+int main() {
+
+	//
+	// 1. Initialisation
+	//
+	
+
+	// Initialise glfw and setup window
+	glfwInit();
+
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+	glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE, GLFW_TRUE);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
+
+	GLFWwindow* window = glfwCreateWindow(initWidth, initHeight, "Real-Time Computer Graphics", NULL, NULL);
+
+	// Check window was created successfully
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window!\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+
+	// Set callback functions to handle different events
+	glfwSetFramebufferSizeCallback(window, resizeWindow); // resize window callback
+	glfwSetKeyCallback(window, keyboardHandler); // Keyboard input callback
+
+
+	// Initialise glew
+	glewInit();
+
+	
+	// Setup window's initial size
+	resizeWindow(window, initWidth, initHeight);
+
+	// Initialise scene - geometry and shaders etc
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // setup background colour to be black
+
+
+	//
+	// 2. Main loop
+	// 
+	
+
+	// Loop while program is not terminated.
+	while (!glfwWindowShouldClose(window)) {
+
+		updateScene();
+		renderScene();						// Render into the current buffer
+		glfwSwapBuffers(window);			// Displays what was just rendered (using double buffering).
+
+		// Poll events (key presses, mouse events)
+		// glfwWaitEvents();				// Use this if no animation.
+		// glfwWaitEventsTimeout(1.0/60.0);	// Use this to animate at 60 frames/sec (timing is NOT reliable)
+		glfwPollEvents();					// Use this version when animating as fast as possible
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+
+
+// renderScene - function to render the current scene
+void renderScene()
+{
+	// Clear the rendering window
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Render objects here...
+}
+
+
+// Function to call when window resized
+void resizeWindow(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);		// Draw into entire window
+}
+
+
+// Function to call to handle keyboard input
+void keyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_PRESS) {
+
+		// check which key was pressed...
+		switch (key)
+		{
+			case GLFW_KEY_ESCAPE:
+				glfwSetWindowShouldClose(window, true);
+				break;
+
+			default:
+			{
+			}
+		}
+	}
+	else if (action == GLFW_RELEASE) {
+		// handle key release events
+	}
+}
+
+
+// Function called to animate elements in the scene
+void updateScene() {
+}
+
